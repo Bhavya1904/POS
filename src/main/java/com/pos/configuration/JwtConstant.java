ae72e1a8a1c9d9240f5c8f8744e0fc7f670058f2
Bhavya1904 <bhavya192004@gmail.com>
2025-12-11 10:42:01 +0530

    Changed JWT secret key

--- src/main/java/com/pos/configuration/JwtConstant.java
+++ src/main/java/com/pos/configuration/JwtConstant.java
@@ -1,7 +1,7 @@
 package com.pos.configuration;
 
-public class JwtConstant {
-    public static final String JWT_SECRET = "23640658a8a87083d8198f357493b101e8334329521a01f52440e9a3053cd34b";
+public class JwtConstant {)
+    public static final String JWT_SECRET = "Your-Secret-Key :)";
 
     public static final String JWT_HEADER = "Authorization";
 
